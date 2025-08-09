5f64d09d072cd04786a45c2bd29aaf9664a0aad1
martinAleB <martbarr@gmail.com>
2025-08-08 21:19:28 -0300

    feat: add time measurement

--- Main.py
+++ Main.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
 import Utils
 import random
+import time
 
 part = Utils.generate_random_particles(20, 40, 2)
-print('Particulas generadas')
+print('Particulas generadas. Ejecutando Cell Index Method...')
+
+inicio = time.time()
 algorithm = Utils.cell_index_method(part, 20, 40, 4, 2)
-print('Algoritmo ejecutado')
+fin = time.time()
+
+print(f'Cell Index Method se ejecutó en {fin - inicio:.4f} segundos')
 
 x = [p.x for p in part]
 y = [p.y for p in part]
